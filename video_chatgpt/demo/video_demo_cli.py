af8ae3a5be86a595adbf87e203650c9e106c2f13
Sushant Gautam <susant.gautam@gmail.com>
2023-08-23 22:29:59 +0200

    evaluation on soccernet chunks

--- video_chatgpt/demo/video_demo_cli.py
+++ video_chatgpt/demo/video_demo_cli.py
@@ -1,101 +1,47 @@
 import os
 import argparse
-import datetime
 import json
-import time
-from video_chatgpt.video_conversation import default_conversation
-from video_chatgpt.utils import (build_logger, violates_moderation, moderation_msg)
-from video_chatgpt.eval.model_utils import initialize_model
-from video_chatgpt.constants import *
+from tqdm import tqdm
+from video_chatgpt.eval.model_utils import initialize_model, load_video
+from video_chatgpt.inference import video_chatgpt_infer
+"""
+
+docker run -it  --network host -v /home/sushant/D1/:/home/sushant/D1/  --gpus=0  --ipc=host --ulimit memlock=-1 --ulimit stack=67108864   nvcr.io/nvidia/pytorch:23.07-py3
+docker run -it  --network host  -v /home/sushant/D1/:/home/sushant/D1/  --device=/dev/kfd --device=/dev/dri --security-opt seccomp=unconfined -u root  --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 rocm/pytorch:rocm5.6_ubuntu20.04_py3.8_pytorch_2.0.1
+
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash && apt-get install git-lfs
 
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Demo")
-
-    parser.add_argument("--host", type=str, default="0.0.0.0")
-    parser.add_argument("--port", type=int)
-    parser.add_argument("--controller-url", type=str, default="http://localhost:21001")
-    parser.add_argument("--concurrency-count", type=int, default=8)
-    parser.add_argument("--model-list-mode", type=str, default="once", choices=["once", "reload"])
-    parser.add_argument("--share", action="store_true")
-    parser.add_argument("--moderate", action="store_true")
-    parser.add_argument("--embed", action="store_true")
-    parser.add_argument("--model-name", type=str, required=True)
-    parser.add_argument("--vision_tower_name", type=str, default="openai/clip-vit-large-patch14")
-    parser.add_argument("--conv-mode", type=str, default="video-chatgpt_v1")
-    parser.add_argument("--projection_path", type=str, required=False, default="")
-
-    args = parser.parse_args()
-
-    return args
-
-args = parse_args()
-
-# Initialize model, tokenizer, etc.
-model, vision_tower, tokenizer, image_processor, video_token_len = \
-    initialize_model(args.model_name, args.projection_path)
-    
-replace_token = DEFAULT_VIDEO_PATCH_TOKEN * video_token_len
-replace_token = DEFAULT_VID_START_TOKEN + replace_token + DEFAULT_VID_END_TOKEN
+cd /home/sushant/D1/SoccerNetExperiments/Soccer-Video-ChatGPT/
+pip install -r requirements.txt
+export PYTHONPATH="./:$PYTHONPATH"
 
 
-# Load video
-# video_path = "video_chatgpt/demo/demo_sample_soccer/ball_out_of_play.mp4"
-# video_path = "video_chatgpt/demo/demo_sample_soccer/foul.mp4"
-# video_path = "video_chatgpt/demo/demo_sample_soccer/goal.mp4"
-# video_path = "video_chatgpt/demo/demo_sample_soccer/injury.mp4"
-# video_path = "video_chatgpt/demo/demo_sample_soccer/throw_in.mp4"
-# video_path = "/home/sushant/SN10s-min5c-50/train/Goal/england_epl__2016-2017__2016-12-27-20-15Liverpool4-1StokeCity__1_224p_2060.55_2070.55|1|Close-UpPlayerOrFieldReferee|Goal.mp4"
+python video_chatgpt/demo/video_demo_cli.py  --model-name ./LLaVA-7B-Lightening-v1-1/  --projection_path  ./video_chatgpt-7B.bin
 
-def upload_video(video_path):
-    _img_list = []
-    # Upload the video to the chatbot
-    llm_message = chat.upload_video(video_path, _img_list)
-    return _img_list
+"""
 
-def add_text(state, text, image, first_run):
-    text = text[:1536]  # Hard cut-off
-    # print("\ntext: ", text)
-    # print("\nstate: ", state)
-    if first_run:
-        text = text[:1200]  # Hard cut-off for videos
-        if '<video>' not in text:
-            text = text + '\n<video>'
-        text = (text, image)
-        state = default_conversation.copy()
-    # print("\ntext1: ", text)
-    # print("\nstate1: ", state)
-    state.append_message(state.roles[0], text)
-    state.append_message(state.roles[1], None)
-    state.skip_next = False
-    # print(f"{first_run}, len: {len(text)}, state: {state.to_gradio_chatbot()}")
-    # print("\nstate2: ", state)
-    # print("first_run: ", first_run)
-    return state
+map_event_type = {
+    "Ball out of play": "out",
+    "Goal": "goal",
+    "Shots": "shot",
+    "Throw-in": "throw-in",
+    "Foul": "foul",
+}
+events_list = "', '".join(set(map_event_type.keys()))
+events_list = f"'{events_list}'"
 
+events_list_formatted = "', '".join(set(map_event_type.values()))
+events_list_formatted = f"'{events_list_formatted}'"
 
-
-# Interactive chat loop
-print("Chatbot initialized. Start chatting (type 'exit' to quit):")
-# user_input = input("You: ")
-
-
-# user_input ='Output in JSON with single key: { "event_type": <goal, foul, ball_throw_in, injury, ball_out_of_play or not_any> }. Your task is to analyze given soccer video clip and accurately determine event type from the optionsd.'
-# while user_input.lower() != 'exit':
-#     state = add_text(state, user_input, img_list, first_run)
-#     *_, results = chat.answer(state, img_list, max_new_tokens=512, first_run=first_run, temperature=0.2)
-#     state, _, img_list, first_run, _, _, _, _, _ = results
-#     # os.system('cls' if os.name == 'nt' else 'clear')
-#     print("\n\n".join([f"User: {entry[0]}\nBot: {entry[1]}" for entry in state.to_gradio_chatbot()]))
-#     user_input = input("You: ")
-
-# print("Chatbot session ended.")
 
 questions = [
-"Deeply analyze the given short soccer game video clip which shows a particular football game event. "
-"No pre-amble and always output answer in JSON with single key 'pred_evnt' and value with the most representative event shown in the video with format: { 'pred_evnt': < only one of 'goal', 'ball_shot_no_goal', 'ball_out_of_boundary', 'ball_throw_in_from_sideline', 'foul_or_injury', 'not_any'> }. ",   
+"Deeply analyze given soccer video clip which shows a particular game event."
+# "Which one of the events from <"+events_list_formatted+"> best matches the event shown in the given video?"
+"No pre-amble and output answer in JSON with single key 'pred_evnt' with one of values from <"+events_list_formatted+"> that best matches event shown in given video. JSON output format: { ''pred_evnt'': ''...'' } ",   
 ]
 
+print(questions[0])
 
 
 from enum import Enum
@@ -108,8 +54,8 @@ class Status(Enum):
 
 import dirtyjson
 
-def getKeyFromState(state, key, value):
-    last_response = state.to_gradio_chatbot()[-1][1].replace("\\", "").replace("\n", "").replace("'", '"') +"}"
+def getKeyFromState(answer, key, value):
+    last_response = ("{"+ answer + "}").replace("\n", '').replace("{{", '{').replace("}}", '}').replace("\\","")
     try:
         parsed_json = dict(dirtyjson.loads(last_response))
         if key in parsed_json:
@@ -118,56 +64,35 @@ def getKeyFromState(state, key, value):
             else:
                 return (Status.WRONG_ANSWER, parsed_json, {"true_answer": value})
         else:
-            return (Status.KEY_NOT_FOUND, parsed_json, {"expected_key": key})
+            return (Status.KEY_NOT_FOUND, parsed_json, {"expected_key": key, "true_answer": value})
     except:
-        return (Status.FAIL_PARSING, last_response)
+        return (Status.FAIL_PARSING, last_response, {"expected_key": key, "true_answer": value})
 
-
-def AskGpt(question, video_path_):
-    user_input = question
-    first_run = True
-    img_list = upload_video(video_path_)
-    first_run= True
-    state= None
-    state = add_text(state, user_input, img_list, first_run)
-    *_, results = chat.answer(state, img_list, max_new_tokens=512, first_run=first_run, temperature=0.2)
-    state, _, img_list, first_run, _, _, _, _, _ = results
-    # print("\n\n".join([f"User: {entry[0]}\nBot: {entry[1]}" for entry in state.to_gradio_chatbot()]))
-    return state
-# ['Ball out of play', 'Goal', 'Shots', 'Throw-in', 'Foul']
-
-map_event_type = {
-    "Ball out of play": "ball_out_of_play",
-    "Goal": "goal",
-    "Shots": "ball_shot_no_goal",
-    "Throw-in": "ball_out_of_boundary",
-    "Foul": "foul_or_injury",
-}
-    # only one of 'goal', 'ball_shot_no_goal', 'ball_out_of_play', 'ball_throw_in_from_sideline', 'foul', 'injury', '
-
+model, vision_tower, tokenizer, image_processor, video_token_len = initialize_model("./LLaVA-7B-Lightening-v1-1/", "./video_chatgpt-7B.bin")
 
 import glob
 import random
+random.seed(42)
 
-vid_path = "/home/sushant/SN10s-min5c-50/train"
+vid_path = "/home/sushant/D1/SoccerNetExperiments/Video-LLaMA-SoccerNet/VideoMAE/SN10s-min5c-50/train"
 mp4_files = glob.glob(f"{vid_path}/**/*.mp4")
 random.shuffle(mp4_files)
-mp4_files = mp4_files[:1]
+
 from video_chatgpt.demo.chat import Chat
 
 all_outputs = []
 for vid_full_path in mp4_files:
-    global chat
-    chat = Chat(args.model_name, args.conv_mode, tokenizer, image_processor, vision_tower, model, replace_token) #reset chat
     fileName = vid_full_path.split("/")[-1].replace(".mp4", "")
     _, ifReplay,CameraType, EventType = fileName.split("|")
     # all_events.append([vid_full_path, ifReplay, CameraType, EventType])
-    print("\nFile: ", [vid_full_path.split("/")[-1]])
-    state_ = AskGpt(questions[0], vid_full_path)
+    # print("\nFile: ", [vid_full_path.split("/")[-1]])
+    video_frames = load_video(vid_full_path)
+    output_answer = video_chatgpt_infer(video_frames, questions[0], 'video-chatgpt_v1', model, vision_tower,
+                                             tokenizer, image_processor, video_token_len)
     true_event = map_event_type.get(EventType, "not_any")
-    output = getKeyFromState(state_, "pred_evnt", true_event)
+    output = getKeyFromState(output_answer, "pred_evnt", true_event)
     print(output)
-    all_outputs.append(output)
+    all_outputs.append(str(output))
 
-print("all_outputs: ", all_outputs)
-# all_event_types = list(set([event[3] for event in all_events]))
+
+print("Output: \n", "\n".join([str(entry) for entry in all_outputs]))
